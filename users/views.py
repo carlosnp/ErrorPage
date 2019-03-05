824b85543288e2f549a9c078212ae676fee6abec
carlosnp <machinery.e@gmail.com>
2019-03-05 19:02:57 -0400

    mejora de los templates errors

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 # Project
 from .models import *
 from .forms import *
@@ -17,16 +19,24 @@ class UserDetailView(DetailView):
 	template_name = 'users/detail.html'
 	queryset = UserModel.objects.all()
 
-class UserCreateView(CreateView):
+class UserCreateView(SuccessMessageMixin, CreateView):
 	template_name = 'users/create.html'
 	form_class = UserModelForm
 	queryset = UserModel.objects.all()
+	success_message = 'Se creo el usuario %(username)s satisfactoriamente'
 	success_url = reverse_lazy("users:list")
 
-class UserUpdateView(UpdateView):
+	def get_success_message(self, cleaned_data):
+		return self.success_message % dict(cleaned_data, username=self.object.username.upper(),)
+
+class UserUpdateView(SuccessMessageMixin, UpdateView):
 	template_name = 'users/create.html'
 	form_class = UserModelForm
 	queryset = UserModel.objects.all()
+	success_message = 'Se actualizo el usuario %(username)s satisfactoriamente'
+
+	def get_success_message(self, cleaned_data):
+		return self.success_message % dict(cleaned_data, username=self.object.username.upper(),)
 	
 	def get_success_url(self):
 		return reverse("users:detail", kwargs={'pk': self.object.pk})
@@ -35,6 +45,11 @@ class UserDeleteView(DeleteView):
 	template_name = 'users/delete.html'
 	queryset = UserModel.objects.all()
 	success_url = reverse_lazy("users:list")
+	success_message = 'Se Elimino el usuario satisfactoriamente'
+
+	def delete(self, request, *args, **kwargs):
+		messages.success(self.request, self.success_message)
+		return super(UserDeleteView, self).delete(request, *args, **kwargs)
 
 class UserDelete(View):
 
